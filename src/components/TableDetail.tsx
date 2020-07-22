7c9316e8512c05ed91d3685059b6c6570c5bb0fd
Sean Henderson <mail@sean.sh>
2020-07-21 20:44:53 -0700

    Populate table detail routes with metric data
    Each route in `/tables/:id` displays the metrics for table with ID of
    `:id` from URI.
    
    Due to time constraints the metric data is hastily styled as a literal
    JSON string in markup.

--- src/components/TableDetail.tsx
+++ src/components/TableDetail.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
-
+import axios from "axios";
+import Card from "react-bootstrap/Card";
 
 // FIXME: need to figure out how to add a real type for render props passed in
 // here. @types/react-router and /history are not working out of the box.
@@ -10,12 +11,53 @@ import { Switch, Route, Link } from "react-router-dom";
 //   match: match<any>;
 //   staticContext?: StaticContext | undefined;
 // };
+
+// FIXME: Type out the expected response
+type AxiosResponse = {
+  data: any;
+};
 function TableDetail(props: any) {
   const { id } = props.match.params;
+
+  // Fetch API data for this table's metrics to State
+  const [ToroData, setToroData] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      return await axios(`https://interview.torodata.io/metrics/${id}`);
+    };
+
+    fetchData().then((response: AxiosResponse) => {
+      console.log(response.data);
+      setToroData(response.data);
+    });
+  }, []);
 
   return (
     <>
       <h1>Table {id}</h1>
+      {ToroData.map((item) => {
+        const name = item.metric;
+        const otherData = {
+          column: item.column,
+          currentValue: item.currentValue,
+        };
+        // FIXME: Took the lazy way out, time constraints
+        const jsonRep = JSON.stringify(otherData, null, "    ");
+        return (
+          <Card key={item.id}>
+            <Card.Body>
+              <Card.Title>
+                <code>{item.metric}</code>
+              </Card.Title>
+              <Card.Text>
+                <code>
+                  <pre>{jsonRep}</pre>
+                </code>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </>
   );
 }
