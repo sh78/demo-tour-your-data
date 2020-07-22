df85df2afba26418bc9e0cd25ee0212538e289c4
Sean Henderson <mail@sean.sh>
2020-07-21 19:55:08 -0700

    Fetch API data and populate Tables list

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 
 type DataTableProps = {
-  title: string
-}
+  title: string;
+};
+
+// FIXME: Type out the expected response
+type AxiosResponse = {
+  data: any;
+};
 
 function DataTable(props: DataTableProps) {
+  // Fetch API data to State
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      return await axios(`https://interview.torodata.io/tables/`);
+    };
+
+    fetchData().then((value: AxiosResponse) => {
+      console.log(value);
+      setData(value);
+    });
+  }, []);
+
   return (
     <>
       <h1>All Tables</h1>
+      {/*data.map((item) => (
+        <li>{item}</li>
+        ))*/}
       <Table responsive striped bordered hover>
         <thead>
           <tr>
@@ -28,7 +50,7 @@ function DataTable(props: DataTableProps) {
             <td>Schema</td>
             <td>Columns</td>
             <td>
-              <Link to="/tables/:id">
+              <Link to={`/tables/${":id"}`}>
                 <Button variant="primary">View</Button>
               </Link>
             </td>
